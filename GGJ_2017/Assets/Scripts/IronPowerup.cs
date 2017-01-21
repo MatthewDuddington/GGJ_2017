1bad75cd6fd77238dc4007227efb99d10c1ed29e
Matthew Duddington <mdudd001@gold.ac.uk>
2017-01-21 15:04:57 +0000

    Adding coin dispersal on collision; Updating ironcladding system

---
+++ GGJ_2017/Assets/Scripts/IronPowerup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronPowerup : MonoBehaviour {
+
+	private static IronPowerup[] ironPowerups = new IronPowerup[10];
+	private static int currentIronPowerupIndex = -1;
+
+	void Start () {
+		enabled = false;
+	}
+	
+	public void Pickup() {
+		enabled = false;
+	}
+
+	public static IronPowerup GetNextIronPowerup() {
+		currentIronPowerupIndex++;
+		if (currentIronPowerupIndex == ironPowerups.Length) {
+			currentIronPowerupIndex = 0;
+		}
+		return ironPowerups[currentIronPowerupIndex];
+	}
+}
