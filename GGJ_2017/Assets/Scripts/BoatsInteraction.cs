ae6e0e03ab9ce21d788b27c2553e8fde2bc2c9fb
witold-gawlowski <witek.gawlowski@gmail.com>
2017-01-21 16:11:18 +0000

    applied force debug

--- GGJ_2017/Assets/Scripts/BoatsInteraction.cs
+++ GGJ_2017/Assets/Scripts/BoatsInteraction.cs
@@ -5,111 +5,102 @@ using UnityEngine;
 
 public class BoatsInteraction : MonoBehaviour {
 
-     public float SqrMinimumDistanceBetweenBoats;
-     public float BoatDurability;
-     public float DamageFromImpactPer1UnitOfSpeed;
-     public float PushBackModifier;
-     private Rigidbody rb;
-     private Vector3 speed;
-     private GameObject collidingObject;
+  public float SqrMinimumDistanceBetweenBoats;
+  public float BoatDurability;
+  public float DamageFromImpactPer1UnitOfSpeed;
+  public float PushBackModifier;
+  private Rigidbody rb;
+  private Vector3 speed;
+  private GameObject collidingObject;
 
-     private float heightThreshold = 1f;
-     public float floatingMultiplier;
-     private wave water;
+  private float heightThreshold = 1f;
+  public float floatingMultiplier;
+  private wave water;
 
-     public float boatHalfHeight;
+  public float boatHalfHeight;
 
-     public Transform[] hullFloatingPoints;
-     float zeroWaterLevel = 0f;
+  public Transform[] hullFloatingPoints;
+  float zeroWaterLevel = 0f;
 
-     // Use this for initialization
-     void Start () {
-          rb = GetComponent<Rigidbody>();
+  // Use this for initialization
+  void Start() {
+    rb = GetComponent<Rigidbody>();
+    collidingObject = null;
+    water = GameObject.FindWithTag("Water").GetComponent<wave>();
+    zeroWaterLevel = water.transform.position.y;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    speed = rb.velocity;
+    simulateFloating();
+  }
+
+  void simulateFloating() {
+    if (water) {
+      //float[] waterLevels = new float[4];
+      foreach (Transform point in hullFloatingPoints) {
+        float waterLevel = water.ProbingFunction(point.position.x, point.position.z, Time.time) - zeroWaterLevel;
+        float currentYLocation = point.position.y;
+        if (currentYLocation < waterLevel) {
+          Vector3 forceAmount = new Vector3(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
+          rb.AddForceAtPosition(forceAmount / 4, point.transform.position);
+          Debug.DrawLine(point.transform.position, point.transform.position + forceAmount / 4);
+          //rb.AddForce(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
+        }
+      }
+
+
+      //GetWaterLevelFunction
+      //getWaterLevelAt(new Vector2(transform.position.x, transform.position.z)) - zeroWaterLevel,
+
+
+      // print("CurrentYLocation = " + currentYLocation + "; waterLevel = " + waterLevel);
+
+
+    }
+  }
+
+  void fourPointsFloating() {
+    float currentYLocation = transform.position.y,
+    waterLevel = water.ProbingFunction(transform.position.x, transform.position.z, Time.time)
+         - zeroWaterLevel;
+    //GetWaterLevelFunction
+    //getWaterLevelAt(new Vector2(transform.position.x, transform.position.z)) - zeroWaterLevel,
+
+
+    // print("CurrentYLocation = " + currentYLocation + "; waterLevel = " + waterLevel);
+
+    if (currentYLocation < waterLevel) {
+      rb.AddForce(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision) {
+    if (collision.gameObject.tag == "Player") {
+      print("Collision!");
+      if (collidingObject) {
+        if ((collidingObject.transform.position - transform.position).sqrMagnitude >= SqrMinimumDistanceBetweenBoats)
           collidingObject = null;
-          water = GameObject.FindWithTag("Water").GetComponent<wave>();
-          zeroWaterLevel = water.transform.position.y;
-     }
-	
-	// Update is called once per frame
-	void Update () {
-          speed = rb.velocity;
-          simulateFloating();
-	}
+      }
+      if (!collidingObject) {
+        collidingObject = collision.gameObject;
+        BoatsInteraction otherBoatScript = collidingObject.GetComponent<BoatsInteraction>();
 
-     void simulateFloating()
-     {
-          if (water)
-          {
-               //float[] waterLevels = new float[4];
-               foreach (Transform point in hullFloatingPoints)
-               {
-                    float waterLevel = water.ProbingFunction(point.position.x, point.position.z, Time.time) - zeroWaterLevel,
-                         currentYLocation = point.position.y;
-                    if (currentYLocation < waterLevel)
-                    {
-                         Vector3 forceAmount = new Vector3(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
-                         rb.AddForceAtPosition(forceAmount / 4, point.transform.position);
-                         //rb.AddForce(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
-                    }
-               }
+        Vector3 otherRBPreviousFrameSpeed = 2 * rb.velocity - speed;
+        Vector3 boatsAxis = collision.rigidbody.transform.position - rb.transform.position;
+        float damageToTheSecond = Vector3.Project(rb.velocity, boatsAxis).magnitude;
+        float damageToTheFirst = Vector3.Project(otherRBPreviousFrameSpeed, boatsAxis).magnitude;
 
+        BoatDurability -= damageToTheFirst * DamageFromImpactPer1UnitOfSpeed;
 
-               //GetWaterLevelFunction
-               //getWaterLevelAt(new Vector2(transform.position.x, transform.position.z)) - zeroWaterLevel,
+        otherBoatScript.BoatDurability -= damageToTheSecond * otherBoatScript.DamageFromImpactPer1UnitOfSpeed;
+        print("Damage1 = " + damageToTheFirst + "; Damage2 = " + damageToTheSecond);
 
-
-              // print("CurrentYLocation = " + currentYLocation + "; waterLevel = " + waterLevel);
-
-
-          }
-     }
-
-     void fourPointsFloating()
-     {
-          float currentYLocation = transform.position.y,
-          waterLevel = water.ProbingFunction(transform.position.x, transform.position.z, Time.time)
-               - zeroWaterLevel;
-          //GetWaterLevelFunction
-          //getWaterLevelAt(new Vector2(transform.position.x, transform.position.z)) - zeroWaterLevel,
-
-
-          // print("CurrentYLocation = " + currentYLocation + "; waterLevel = " + waterLevel);
-
-          if (currentYLocation < waterLevel)
-          {
-               rb.AddForce(0f, (waterLevel - currentYLocation) * floatingMultiplier + boatHalfHeight, 0f);
-          }
-     }
-
-     private void OnCollisionEnter(Collision collision)
-     {
-          if (collision.gameObject.tag == "Player")
-          {
-               print("Collision!");
-               if (collidingObject)
-               {
-                    if ((collidingObject.transform.position - transform.position).sqrMagnitude >= SqrMinimumDistanceBetweenBoats)
-                         collidingObject = null;
-               }
-               if (!collidingObject)
-               {
-                    collidingObject = collision.gameObject;
-                    BoatsInteraction otherBoatScript = collidingObject.GetComponent<BoatsInteraction>();
-
-                    Vector3 otherRBPreviousFrameSpeed = 2 * rb.velocity - speed;
-                    Vector3 boatsAxis = collision.rigidbody.transform.position - rb.transform.position;
-                    float damageToTheSecond = Vector3.Project(rb.velocity, boatsAxis).magnitude;
-                    float damageToTheFirst = Vector3.Project(otherRBPreviousFrameSpeed, boatsAxis).magnitude;
-
-                    BoatDurability -= damageToTheFirst * DamageFromImpactPer1UnitOfSpeed;
-
-                    otherBoatScript.BoatDurability -= damageToTheSecond * otherBoatScript.DamageFromImpactPer1UnitOfSpeed;
-                    print("Damage1 = " + damageToTheFirst + "; Damage2 = " + damageToTheSecond);
-
-                    print("Normal = " + collision.contacts[0].normal);
-                    rb.AddForceAtPosition(collision.impulse.magnitude * PushBackModifier * collision.contacts[0].normal, collision.contacts[0].point);
-                    collision.rigidbody.AddForceAtPosition(collision.impulse.magnitude * PushBackModifier * -collision.contacts[0].normal, collision.contacts[0].point);
-               }
-          }
-     }
+        print("Normal = " + collision.contacts[0].normal);
+        rb.AddForceAtPosition(collision.impulse.magnitude * PushBackModifier * collision.contacts[0].normal, collision.contacts[0].point);
+        collision.rigidbody.AddForceAtPosition(collision.impulse.magnitude * PushBackModifier * -collision.contacts[0].normal, collision.contacts[0].point);
+      }
+    }
+  }
 }
