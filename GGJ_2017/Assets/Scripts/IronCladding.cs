97ae96a2b7a97f8e405999960fa3e905c8aed8f7
Matthew Duddington <mdudd001@gold.ac.uk>
2017-01-21 03:08:40 +0000

    Coin & Ironcladding pickups and PlayerScript integration

---
+++ GGJ_2017/Assets/Scripts/IronCladding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronCladding : MonoBehaviour {
+
+	private float powerupTime_ = 6;
+
+	public float PowerupTime() {
+		return powerupTime_;
+	}
+
+	public void Pickup() {
+		// TODO Playsound "KLANG Hammer Hammer"
+	}
+}
--- GGJ_2017/Assets/Scripts/PlayerScript.cs
+++ GGJ_2017/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-     public float CharacterSpeed;
-     public float JumpStrength;
+    public float CharacterSpeed;
+    public float JumpStrength;
 
-     private Rigidbody rb;
+    private Rigidbody rb;
 
-     private bool moving;
-     private Vector3 destination;
+    private bool moving;
+    private Vector3 destination;
 
-     public bool IsControllable;
+    public bool IsControllable;
+
+    private int CoinTotal = 0;
+	private int numberOfCoinsToDropWhenHit = 10;
+
+    private bool isIronclad_;
 
 
 	// Use this for initialization
@@ -44,5 +49,47 @@ public class PlayerScript : MonoBehaviour {
                if (Input.GetKeyDown(KeyCode.Space))
                     rb.AddForce(0f, JumpStrength, 0f);
           }
-     }
+    }
+	
+	void OnCollisionEnter(Collision coll) {
+		GameObject other = coll.gameObject;
+		if (other is Coin) {
+			PickupCoins(other.GetComponent<Coin>().Pickup());
+		}
+		else if (other is IronCladding) {
+			other.GetComponent<IronCladding>().Pickup());
+			StartCoroutine(IroncladPowerTimer());
+		}
+		else if (other.gameObject is PlayerScript) {
+			if (other.gameObject.GetComponent<PlayerScript>().IsIronclad()) {
+				DropCoins(numberOfCoinsToDropWhenHit);
+			}
+			else {
+				// TODO Playsound "BOING"
+			}
+		}
+	}
+
+    public void PickupCoins(int numberOfCoins) {
+    	CoinTotal += numberOfCoins;
+    	// TODO Update UI
+    }
+
+    public bool IsIronclad() {
+    	return isIronclad_;
+    }
+
+    private void DropCoins(int numberOfCoins) {
+    	CoinTotal -= numberOfCoins;
+    	// TODO Spawn a set of coins around the boat (use Coin.ThrowAway() after instantiating)
+    	// TODO Update UI
+    	// TODO Playsound "Clink clank kaplunk"
+    }
+
+    IEnumerator IroncladPowerTimer() {
+    	isIronclad_ = true;
+    	// TODO Playsound "Dun dun dun dun dun dun... (Jaws)"
+    	yield return new WaitForSeconds(IronCladding.PowerupTime());
+    	isIronclad_ = false;
+    }
 }
