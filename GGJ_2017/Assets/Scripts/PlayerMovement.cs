03f58e7114ff0cfd6f7d9ffc705bb8afefe0f02a
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-21 02:52:53 +0000

    Updating the players movement and the wave generation to the plane mesh instead of cubes

---
+++ GGJ_2017/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movement_speed;
+    private Rigidbody rb;
+
+    private float hori_axis;
+    private float vert_axis; 
+
+
+    public float forward_speed;
+
+    private Vector3 player_rotation;
+    private Vector3 joystick_position; 
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        hori_axis = Input.GetAxis("Horizontal");
+        vert_axis = Input.GetAxis("Vertical");
+
+        //forward_speed = Input.GetAxis("Horizontal");
+        //rb.AddForce(0f, 0f, forward_speed * movement_speed);
+
+        if (hori_axis >= 0.3f || vert_axis >= 0.3)
+        {
+        rb.AddRelativeForce(movement_speed, 0.0f, 0.0f);
+
+        }
+
+
+        joystick_position = new Vector3(hori_axis, 0.0f, vert_axis);
+        player_rotation = transform.forward;
+
+        //player_rotation = Mathf.Atan(hori_axis / vert_axis);
+
+        //rb.AddTorque(Vector3.Lerp(player_rotation, joystick_position, 0.1f));
+        //transform.Rotate();
+        transform.forward = Vector3.Lerp(player_rotation, joystick_position, 0.01f);
+        
+        print(Vector3.Lerp(player_rotation, joystick_position, 0.5f));
+    }
+}
---
+++ GGJ_2017/Assets/wave.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wave : MonoBehaviour
+{
+    public float scale = 0.1f;
+    public float speed = 1.0f;
+    public float length = 10.0f;
+    public float noiseStrength = 1f;
+    public float noiseWalk = 1f;
+    Vector3 position;
+
+
+    private Vector3[] baseHeight;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        position = transform.position;
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        if (baseHeight == null)
+            baseHeight = mesh.vertices;
+
+        Vector3[] vertices = new Vector3[baseHeight.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = baseHeight[i];
+            
+            //vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z + position.x + position.y + position.z) * scale;
+            vertex.y = Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].z + position.x  + position.z) * scale;
+
+            //vertex.y += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+            vertices[i] = vertex;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
