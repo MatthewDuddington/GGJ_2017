2d942e77b8dcb87b4bf8be79086bf98699688b2f
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-22 08:40:02 +0000

    Adding a death scene and the other lives mechanic

---
+++ GGJ_2017/Assets/Scripts/PlayerLifes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifes : MonoBehaviour {
+
+    public int lives = 5; 
+
+    public void loseLife()
+    {
+        lives--; 
+    }
+
+    public int getLife()
+    {
+        return lives;
+    }
+}
---
+++ GGJ_2017/Assets/Scripts/respawn_collider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class respawn_collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.GetComponent<PlayerLifes>() != null)
+        {
+            collision.GetComponent<PlayerLifes>().loseLife();
+            if (collision.GetComponent<PlayerLifes>().getLife() == 0)
+            {
+                // Change game mode to End Game
+                SceneManager.LoadScene(0, LoadSceneMode.Single);
+            }  
+        }
+           
+        
+    }
+}
