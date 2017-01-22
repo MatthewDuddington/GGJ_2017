154102af0a7fdd7854cf9d77f864d47b5a696605
MSI\robert <robertjdoig@hotmail.co.uk>
2017-01-22 00:01:34 +0000

    Adding the UI example level

---
+++ GGJ_2017/Assets/Scripts/UI_Scripts/change_scene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class change_scene : MonoBehaviour {
+    public void set_scene_ID(int ID)
+    {
+        SceneManager.LoadScene(ID, LoadSceneMode.Single);
+    }
+}
---
+++ GGJ_2017/Assets/get_player_score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_score : MonoBehaviour {
+
+    public float player1_score = 0.1f;
+
+    private Text txtRef;
+    private void Awake()
+    {
+        txtRef = GetComponent<Text>();//or provide from somewhere else (e.g. if you want via find GameObject.Find("CountText").GetComponent<Text>();)
+    }
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+	void Update () {
+    //then where you need:
+    txtRef.text = "Player 1: " + player1_score;
+	}
+}
